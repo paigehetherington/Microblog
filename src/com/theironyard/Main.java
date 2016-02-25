df80294627e36a5f2eb5187eaf1451c90bbed326
Paige Hetherington <herbmama@gmail.com>
2016-02-24 20:51:12 -0500

    replaced static user object

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -10,14 +10,17 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
 
-    static User user;
+    //static User user;
 
     public static void main(String[] args) {
         HashMap<String, User> allUsers = new HashMap<>();
         User doug = new User("Doug", "1234");
         allUsers.put(doug.name, doug);
+
         Spark.staticFileLocation("public"); //tells server where files are for CSS
+
         Spark.init();
+
         Spark.get( //method call takes 3 arguments, split on 3 lines
                 "/",
                 ((request, response) -> { //anonymous fxn
@@ -47,12 +50,12 @@ public class Main {
                     String name = request.queryParams("indexName");
                     String password = request.queryParams("password");
                     if (!allUsers.containsKey(name)) {
-                        user = new User(name, password);
+                        User user = new User(name, password);
                         allUsers.put(name, user);
                         response.redirect("/");
                     } else {
                         if (password.equals(allUsers.get(name).password)){
-                            user = new User (name, password);
+                            //user = new User (name, password);
                             response.redirect("/");
                         } else {
                             Spark.halt(403);
@@ -75,19 +78,27 @@ public class Main {
                 })
 
         );
+
         Spark.post(
             "/create-message",
                 ((request, response) ->  {
+                    Session session = request.session();
+                    String name = session.attribute("userName");
+                    User user = allUsers.get(name);
                     String text = request.queryParams("newMessage");
                     Message message = new Message(text);
-                    user.messages.add(message);
+                    user.messages.add(message); //add to AL of messages
                     response.redirect("/");
                     return "";
                 })
         );
+
         Spark.post(
                 "/delete-message",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("userName");
+                    User user = allUsers.get(name);
                     String deleteNum = request.queryParams("deleteNum");
                     int idNum = Integer.valueOf(deleteNum);
                     user.messages.remove(idNum -1);
@@ -95,9 +106,13 @@ public class Main {
                     return "";
                 })
         );
+
         Spark.post(
                 "/edit-message",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("userName");
+                    User user = allUsers.get(name);
                     String editNum = request.queryParams("editNum");
                     int idNum = Integer.valueOf(editNum);
                     Message edit = user.messages.get(idNum - 1);
@@ -109,6 +124,7 @@ public class Main {
 
                 })
         );
+
         Spark.post(
                 "/logout",
                 ((request, response) ->  {
